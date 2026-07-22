1b9e908051f079812daaf4d142a703320c320122
Mariana Rodrigues <marianapvh2010@gmail.com>
2026-07-22 14:48:44 -0400

    fix: ajusta quest03 de acordo com revisão.

--- ruby_quests/quest03.rb
+++ ruby_quests/quest03.rb
@@ -1,13 +1,15 @@
 print "Digite seu nome: "
 nome = gets.chomp
+
 print "Qual sua cidade: "
 cidade = gets.chomp
-print "Qual sua idade: "
+
+print "Qual o ano do seu nascimento: "
 ano = gets.chomp.to_i
 
 puts "Olá #{nome}"
 puts "Você mora em #{cidade}"
-puts "Você nasceu aproximadamente em #{2026 - ano}"
+puts "Você nasceu em #{ano}"
 
 print "Qual é sua profissão: "
 profissao = gets.chomp
@@ -15,5 +17,5 @@ profissao = gets.chomp
 puts "=== Apresentação ==="
 puts "Olá! Meu nome é #{nome}."
 puts "Moro em #{cidade} e atuo como #{profissao}."
-puts "Nasci em #{2026 - ano}."
-puts "Então minha idade aproximada é #{ano} anos."
+puts "Nasci em #{ano}."
+puts "Então minha idade é de #{2026 - ano} anos."
